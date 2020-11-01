ac171feceeb1abd959e368d93d97c9afb75c3b99
mpucci92 <42786192+mpucci92@users.noreply.github.com>
2020-10-31 21:38:31 -0400

    Add files via upload

--- src/algo/RandomForestClassifier_McGill_v1.py
+++ src/algo/RandomForestClassifier_McGill_v1.py
@@ -8,13 +8,14 @@ from datetime import datetime as dt
 from dateutil.relativedelta import *
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import balanced_accuracy_score
+
+
 import warnings
 warnings.filterwarnings("ignore")
 
-# from datetime import datetime, timedelta
-
 from yahoo_fin import stock_info as si
 
+validation_scores_allticks = []
 
 def get_last_stock_price(ticker, last=False):
     #if last:
@@ -31,16 +32,35 @@ def features_creation(df):
         daily_change_OC.append(100 * (df.adjclose.iloc[i] - df.open.iloc[i]) / (df.open.iloc[i]))
 
     df['daily percentage change'] = daily_change_OC
+    daily_pct_change = df['daily percentage change'].pct_change()
+    df['vol_dailychange'] = daily_pct_change
+
+    volume_change = df['volume'].pct_change()
+    df['volume_change'] = volume_change
 
     df = df.fillna(0)
 
     df['ma8'] = df['adjclose'].rolling(window=8).mean()
     df['ma12'] = df['adjclose'].rolling(window=12).mean()
     df['ma20'] = df['adjclose'].rolling(window=20).mean()
+    df['ma50'] = df['adjclose'].rolling(window=50).mean()
 
     df['std20'] = df['adjclose'].rolling(window=20).std()
     df['skew20'] = df['adjclose'].rolling(window=20).skew()
     df['kurt20'] = df['adjclose'].rolling(window=20).kurt()
+
+    df['auto1'] = df['adjclose'].rolling(1).apply(lambda x: pd.Series(x).autocorr())
+    df['auto2'] = df['adjclose'].rolling(2).apply(lambda x: pd.Series(x).autocorr())
+    df['auto3'] = df['adjclose'].rolling(3).apply(lambda x: pd.Series(x).autocorr())
+    df['auto4'] = df['adjclose'].rolling(4).apply(lambda x: pd.Series(x).autocorr())
+    df['auto5'] = df['adjclose'].rolling(5).apply(lambda x: pd.Series(x).autocorr())
+
+    vol30 = df['vol_dailychange'].rolling(30).std() * np.sqrt(30)
+    vol90 = df['vol_dailychange'].rolling(90).std() * np.sqrt(90)
+    df['30D_vol'] = vol30
+    df['90D_vol'] = vol90
+
+    df = df.fillna(0)
 
     return df
 
@@ -60,23 +80,13 @@ def label_creation(df):
 
 def preprocessing(df):
     scaler = MinMaxScaler()
-    columns = ['open', 'high', 'low', 'close', 'adjclose', 'volume', 'ma8', 'ma12', 'ma20']
+    columns = ['adjclose', 'ma8', 'ma12', 'ma20',
+               'ma50']
 
     for col in columns:
         df[col] = scaler.fit_transform(df[col].values.reshape(-1, 1))
 
     return df
-
-
-
-# df = features_creation(df)
-# df = label_creation(df)
-# preprocessing(df)
-# df = df[df.timestamp > '2010-01-01']
-# df_train_test = df[df.timestamp < '2020-11-11']
-#
-# XY_train = df_train_test.loc[:,['timestamp','adjclose','volume','ma8','ma12','ma20','std20','skew20','kurt20','labels']]
-#
 
 class TimeBasedCV(object):
     '''
@@ -91,10 +101,10 @@ class TimeBasedCV(object):
     freq: string
         frequency of input parameters. possible values are: days, months, years, weeks, hours, minutes, seconds
         possible values designed to be used by dateutil.relativedelta class
-        deafault is days
+        default is days
     '''
 
-    def __init__(self, train_period=30, test_period=7, freq='days'):
+    def __init__(self, train_period=100, test_period=50, freq='days'):
         self.train_period = train_period
         self.test_period = test_period
         self.freq = freq
@@ -196,13 +206,14 @@ def main_function(ticker):
     df_train_test = df[df.timestamp < end_date]  # End Date
 
     XY_train = df_train_test.loc[:,
-               ['timestamp', 'adjclose', 'volume', 'ma8', 'ma12', 'ma20', 'std20', 'skew20', 'kurt20', 'labels']]
+               ['timestamp', 'adjclose', 'ma8', 'ma12', 'ma20', 'std20', 'skew20', 'kurt20',
+    '30D_vol', '90D_vol', 'auto1', 'auto2', 'auto3', 'auto4', 'auto5', 'ma50','labels']]
 
     data_for_modeling = XY_train
-    tscv = TimeBasedCV(train_period=25,
-                       test_period=10,
+    tscv = TimeBasedCV(train_period=20,
+                       test_period=5,
                        freq='days')
-    for train_index, test_index in tscv.split(data_for_modeling, validation_split_date=datetime.date(2019, 2, 1),
+    for train_index, test_index in tscv.split(data_for_modeling, validation_split_date=datetime.date(2018, 1, 21),
                                               date_column='timestamp'):
         # get number of splits
         print(train_index, test_index)
@@ -210,14 +221,15 @@ def main_function(ticker):
     tscv.get_n_splits()
 
     #### Example- compute average test sets score: ####
-    X = data_for_modeling[['timestamp', 'adjclose', 'volume', 'ma8', 'ma12', 'ma20', 'std20', 'skew20', 'kurt20']]
+    X = data_for_modeling[['timestamp', 'adjclose', 'ma8', 'ma12', 'ma20', 'std20', 'skew20', 'kurt20',
+                           '30D_vol', '90D_vol','auto1','auto2','auto3','auto4','auto5','ma50']]
     y = data_for_modeling['labels']
 
     ##############################
 
     scores = []
 
-    for train_index, test_index in tscv.split(X, validation_split_date=datetime.date(2019, 2, 1)):
+    for train_index, test_index in tscv.split(X, validation_split_date=datetime.date(2018, 1, 21)):
         data_train = X.loc[train_index].drop('timestamp', axis=1)
         target_train = y.loc[train_index]
 
@@ -227,16 +239,27 @@ def main_function(ticker):
         # if needed, do preprocessing here
 
         # Create a Gaussian Classifier
-        clf = RandomForestClassifier(n_estimators=100)
+        clf = RandomForestClassifier(n_estimators=50,criterion = 'gini', max_depth = 5,
+                                     min_samples_split = 2, min_samples_leaf = 1,max_features = 'sqrt')
+
+        # param_grid = {
+        #      'n_estimators': [50,100,150],
+        #      'max_features': ['auto', 'sqrt'],
+        #      'criterion' :  ['gini', 'entropy'],
+        #      'max_depth' : [None,5,10,15]}
+
 
         # Train the model using the training sets y_pred=clf.predict(X_test)
+        #clf.fit(data_train, target_train)
+        # clf_opt = GridSearchCV(clf, param_grid=param_grid, scoring=f1_scorer, cv=5)
         clf.fit(data_train, target_train)
 
         preds = clf.predict(data_test)
         scores.append(balanced_accuracy_score(target_test, preds))
 
-    average_r2score = np.mean(scores)
-    print("Balanced Accuracy: %f" % (100 * average_r2score))
+    average_BAscore = np.mean(scores)
+    print("Balanced Accuracy: %f" % (100 * average_BAscore))
+    # print("Best Parameters: %s" % (clf_opt.best_params_))
 
     predictions = []
     true_inputs = []
@@ -245,7 +268,8 @@ def main_function(ticker):
     df_valid = df[df.timestamp > end_date]
     df_valid = df_valid.drop(['timestamp'], axis=1)
     for i in range(len(df_valid)):
-        val = df_valid.loc[:, ['adjclose', 'volume', 'ma8', 'ma12', 'ma20', 'std20', 'skew20', 'kurt20']]
+        val = df_valid.loc[:, ['adjclose', 'ma8', 'ma12', 'ma20', 'std20', 'skew20', 'kurt20',
+                               '30D_vol', '90D_vol', 'auto1', 'auto2', 'auto3', 'auto4', 'auto5', 'ma50']]
         pred_input = clf.predict(val.iloc[i].values.reshape(1, -1))
         predictions.append(int(pred_input[0]))
 
@@ -256,6 +280,8 @@ def main_function(ticker):
         validation_scores.append(balanced_accuracy_score(true_input, pred_input))
 
     print("Validation Balanced Accuracy: %f" % (100 * np.mean(validation_scores)))
+    validation_scores_allticks.append(np.mean(validation_scores))
+
     if pred_input == 1:
         print('Tomorrow Predicition: BUY')
         positions = "BUY"
@@ -272,85 +298,3 @@ def main_function(ticker):
 
 
 
-# if __name__ == '__main__':
-#     main_function('AAPL')
-
-    # df = pd.read_csv(r"C:\Users\mpucci\Desktop\McGill Final Documents\Stock Project\stock\data\AAPL_2020_10_12.csv")
-    #
-    # df.columns = ['timestamp', 'open', 'high', 'low', 'close', 'adjclose', 'volume', 'ticker']
-    #
-    # df['timestamp'] = pd.to_datetime(df.timestamp)
-    #
-    # start_date = '2018-01-01'
-    # end_date =  '2020-01-01'
-    #
-    # df = features_creation(df)
-    # df = label_creation(df)
-    # preprocessing(df)
-    # df = df[df.timestamp > start_date]                     # Start Date
-    # df_train_test = df[df.timestamp < end_date]          # End Date
-    #
-    # XY_train = df_train_test.loc[:,
-    #            ['timestamp', 'adjclose', 'volume', 'ma8', 'ma12', 'ma20', 'std20', 'skew20', 'kurt20', 'labels']]
-    #
-    # data_for_modeling = XY_train
-    # tscv = TimeBasedCV(train_period=25,
-    #                    test_period=10,
-    #                    freq='days')
-    # for train_index, test_index in tscv.split(data_for_modeling,validation_split_date=datetime.date(2019, 2, 1), date_column='timestamp'):
-    #     #get number of splits
-    #     print(train_index, test_index)
-    #
-    # tscv.get_n_splits()
-    #
-    # #### Example- compute average test sets score: ####
-    # X = data_for_modeling[['timestamp', 'adjclose', 'volume', 'ma8', 'ma12', 'ma20', 'std20', 'skew20', 'kurt20']]
-    # y = data_for_modeling['labels']
-    #
-    # ##############################
-    #
-    # scores = []
-    #
-    # for train_index, test_index in tscv.split(X, validation_split_date=datetime.date(2019, 2, 1)):
-    #     data_train = X.loc[train_index].drop('timestamp', axis=1)
-    #     target_train = y.loc[train_index]
-    #
-    #     data_test = X.loc[test_index].drop('timestamp', axis=1)
-    #     target_test = y.loc[test_index]
-    #
-    #     # if needed, do preprocessing here
-    #
-    #     # Create a Gaussian Classifier
-    #     clf = RandomForestClassifier(n_estimators=100)
-    #
-    #     # Train the model using the training sets y_pred=clf.predict(X_test)
-    #     clf.fit(data_train, target_train)
-    #
-    #     preds = clf.predict(data_test)
-    #     scores.append(balanced_accuracy_score(target_test, preds))
-    #
-    # average_r2score = np.mean(scores)
-    # print("Balanced Accuracy: %f" % (100*average_r2score))
-    #
-    # predictions = []
-    # true_inputs = []
-    # validation_scores = []
-    #
-    # df_valid = df[df.timestamp > end_date]
-    # df_valid = df_valid.drop(['timestamp'], axis=1)
-    # for i in range(len(df_valid)):
-    #     val = df_valid.loc[:, ['adjclose', 'volume', 'ma8', 'ma12', 'ma20', 'std20', 'skew20', 'kurt20']]
-    #     pred_input = clf.predict(val.iloc[i].values.reshape(1, -1))
-    #     predictions.append(int(pred_input[0]))
-    #
-    #     true = df_valid.loc[:, ['labels']]
-    #     true_input = true.iloc[i].values
-    #
-    #     true_inputs.append(int(true_input[0]))
-    #     validation_scores.append(balanced_accuracy_score(true_input, pred_input))
-    #
-    # print("Validation Balanced Accuracy: %f" % (100*np.mean(validation_scores)))
-    # if pred_input == 1:
-    #     print('Tomorrow Predicition: BUY')
-    # elif pred_input == -1:
-    #     print('Tomorrow Predicition: SELL')
